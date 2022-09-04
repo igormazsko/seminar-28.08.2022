fba0875653a0915a253ca8f156eb61aef22f3a42
Igor Matsko <igormazsko@mail.ru>
2022-09-04 16:48:34 +0300

    add Task 55

---
+++ Task/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите через пробел размеры массива: ");
+int [] parametrs = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(x=> int.Parse(x)).ToArray();
+int [,] array = GetMatrix(parametrs[0], parametrs[1]);
+PrintMatrix(array);
+WriteLine();
+int [,] array1 = ExchangeOfLines(array);
+PrintMatrix(array1);
+
+
+
+
+int [,]GetMatrix(int rows, int columns)
+{
+    int [,] arr = new int [rows, columns];
+    for (int i = 0; i<rows; i++)
+    {
+        for (int j = 0; j<columns; j++)
+        {
+            arr[i,j] = new Random ().Next(1,100);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,]matrixForPrint)
+{
+    for (int i = 0; i<matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j<matrixForPrint.GetLength(1); j++)
+        {
+                Write($"{matrixForPrint[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int [,] ExchangeOfLines(int[,] arr)
+{
+    int [,] newarr = arr;
+           for (int j = 0; j<arr.GetLength(1); j++)
+        {
+            int variable = arr [0,j];
+            newarr[0,j] = arr [arr.GetLength(0)-1, j];
+            newarr [arr.GetLength(0)-1, j] = variable;
+        }
+    return newarr;
+}
---
+++ Task_55/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна вывести сообщение для пользователя.*/
+using static System.Console;
+using System.Linq;
+Clear();
+Write("Введите количество строк в матрице m= ");
+int m = int.Parse(ReadLine());
+Write("Введите количество столбцов в матрице n= ");
+int n = int.Parse(ReadLine());
+int [,]matrix= new int [m, n];
+FillMassRandomNambers(matrix);
+PrintMatrixArray(matrix);
+WriteLine();
+if (matrix.GetLength(0)!=matrix.GetLength(1))
+{
+    WriteLine("Поменять местами строи и столбцы невозможно");
+    return;
+}
+else
+{
+    PrintMatrixArray(RowsPerColumn(matrix));
+}
+
+// Метод заполнения двухмерного массива случайными числами
+void FillMassRandomNambers (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+// Метод печати двухмерного массива заполненного вещественными значениями
+void PrintMatrixArray(int [,] inArray)
+{
+    for (int i = 0; i<inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray [i,j]} ");
+        }
+        WriteLine();
+    }
+}
+// Метод замены строк на столбцы
+int [,] RowsPerColumn (int [,] array)
+{
+    int [,] newArray = new int [array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[i,j]=array[j,i];
+        }
+    }
+    return newArray;
+}
